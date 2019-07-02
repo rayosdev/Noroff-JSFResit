8036d4fafe6bfdaf859d3e21f13d6960000c7799
Rayos <rayos.dev@gmail.com>
2019-07-02 05:38:54 +0200

    fixing links in header

--- noroff-jsfresit/src/App.vue
+++ noroff-jsfresit/src/App.vue
@@ -1,22 +1,64 @@
 <template>
+<div class="main-container">
+
   <div id="app" class="header__container">
-    <div id="nav" class="header__nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/contact">Contact</router-link>
+    <div class="header__content_container">
+      <img alt="" src="./assets/header-logo.png">
+      
+      <div class="header__nav-container">
+        <img class="header__nav-bg-image" alt="" src="./assets/header-nav-card-graphics.svg">
+        <div id="nav" class="header__nav-link-container">
+          <router-link class="header__nav-link" to="/">Home</router-link>
+          <router-link class="header__nav-link" to="/grass">Grass Only</router-link>
+          <router-link class="header__nav-link" to="/contact">Contact</router-link>
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
+  <router-view/>
+</div>
 </template>
-<style lang="stylus">
+<style lang="stylus" scoped>
 
 
 .header
 
   &__container
+    background #525252
+    max-height 155px
+  
+  &__content_container
+    max-width 1152px
     margin 0 auto
-    max-width 
+    width 100%
+    display flex
+    justify-content space-between
 
+  &__nav-container
+    display grid
+
+  &__nav-bg-image
+    grid-row 1/2
+    grid-column 1/2
+
+  &__nav-link-container
+    grid-row 1/2
+    grid-column 1/2
+    display grid
+    grid-template-columns 1fr 1fr 1fr
+
+  &__nav-link
+    font-family: Rosario;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 27px;
+    line-height: 33px;
+    text-align: center;
+    text-decoration-line: underline;
+    color: #F6D301;
+
+    align-self center
+    margin-bottom 1em
 
 // #app
 //   font-family 'Avenir', Helvetica, Arial, sans-serif
@@ -32,5 +74,19 @@
 //     color #2c3e50
 //     &.router-link-exact-active
       // color #42b983
+</style>
+
+<style lang="stylus">
+// Global styles
+
+body
+  margin 0
+  padding 0
+
+a
+  color #fff
+  
 
 </style>
+
+
