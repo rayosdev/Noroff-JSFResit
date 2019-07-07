090a0cd3df7413dc0821feeebd1a7c8d6f56fa8e
Rayos <rayos.dev@gmail.com>
2019-07-07 23:58:20 +0200

    last push

--- noroff-jsfresit/src/views/Contact.vue
+++ noroff-jsfresit/src/views/Contact.vue
@@ -2,11 +2,19 @@
     <div class="main-container">
         <form action="" class="contact-form__container">
 
-            <label for="name">
-                <input type="text" id="name" class="contact-form__name">
+            <label class="contact-form__name" for="name">
+                <input type="text" id="name" class="contact-form__name-input">
+            </label>
+            <label class="contact-form__phone" for="phone">
+                <input type="text" id="phone" class="contact-form__phone-input">
             </label>
             <label for="phone">
-                <input type="text" id="phone" class="contact-form__phone">
+                <select>
+                    <option value="volvo">Enquiry</option>
+                    <option value="saab">Complaint</option>
+                    <option value="mercedes">Compliment</option>
+                    <option value="audi">General Message</option>
+                </select>
             </label>
 
         </form>
